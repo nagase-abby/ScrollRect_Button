775b30d4bf97526653e2b8bbea638e0036cbb180
nagase-abby <nagase@abby.co.jp>
2024-09-19 16:46:31 +0900

    ボタン押下時の処理修正

--- Assets/Scenes/TestScene.cs
+++ Assets/Scenes/TestScene.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TestScene : MonoBehaviour
 {
     [SerializeField]
     private Button[] button = null;
 
+    [SerializeField]
+    private TextMeshProUGUI _text = null;
+
     private void Awake()
     {
         for (int i = 0; i < button.Length; i++)
         {
+            int no = i;
             button[i].onClick.AddListener(() =>
             {
-                Debug.Log("押した");
+                _text.text = $"number: {no} button click!";
             });
         }
     }
